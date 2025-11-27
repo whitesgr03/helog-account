fb27aae1f201c24b955a8cfe484053e1474ee19e
Weiss Bai <whitesgr03@gmail.com>
2025-11-27 16:57:32 +0800

    feat: create layout components

---
+++ src/components/layout/footer/Footer.tsx
@@ -0,0 +1,22 @@
+// Styles
+import styles from './Footer.module.css';
+import imageStyles from '../../../styles/image.module.css';
+
+export const Footer = () => {
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.container}>
+				<a href="https://github.com/whitesgr03/helog" className={styles.link}>
+					<span className={`${imageStyles.icon} ${styles.github}`} />
+				</a>
+				<address>
+					<a href="mailto:whitesgr03@gmail.com" className={styles.link}>
+						<span className={`${imageStyles.icon} ${styles.email}`} />
+						<em className={styles.emphasis}>whitesgr03@gmail.com</em>
+					</a>
+				</address>
+			</div>
+			<p>© 2025 Designed & coded by Weiss Bai</p>
+		</footer>
+	);
+};
---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+export const Header = () => {
+	return (
+		<header className={styles.header}>
+			<a className={styles.link} href={import.meta.env.VITE_HELOG_URL}>
+				<h1 className={styles.logo}>HeLog Account</h1>
+			</a>
+		</header>
+	);
+};
