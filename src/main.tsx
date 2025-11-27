e588d353efae9ef7fd86b8d49cd6359a4a4dea1f
Weiss Bai <whitesgr03@gmail.com>
2025-11-27 16:56:26 +0800

    feat: render routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './components/App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.css';
+
+import { Router } from './router.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<Router />
+	</StrictMode>,
+);
