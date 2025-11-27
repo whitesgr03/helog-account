a935c809e885de3c0583ef02d95a06bd013fd66b
Weiss Bai <whitesgr03@gmail.com>
2025-11-27 20:08:45 +0800

    feat: app component fetched user's data

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -6,18 +6,43 @@ import styles from './App.module.css';
 import { Footer } from '../layout/footer/Footer';
 import { Header } from '../layout/header/Header';
 
+import { Loading } from '../utils/Loading';
+import { ErrorComponent } from '../utils/Error/Error';
+
+import { useFetchUser } from '../useFetchUser';
+
 export const App = () => {
+	const { user, error, isLoading } = useFetchUser();
+
+	const isError =
+		error &&
+		(error.cause instanceof Error ||
+			(error.cause instanceof Response && error.cause.status !== 401));
+
 	return (
 		<div className={styles.app}>
 			<ScrollRestoration getKey={location => location.key} />
-			<div className={styles['header-bar']}>
-				<Header />
-			</div>
-			<div className={styles.container}>
-				<Outlet />
-
-				<Footer />
-			</div>
+			{isError ? (
+				<ErrorComponent />
+			) : user ? (
+				<div className={styles.loading}>
+					<Loading text="Redirecting to home page..." />
+				</div>
+			) : isLoading ? (
+				<div className={styles.loading}>
+					<Loading text="Loading..." />
+				</div>
+			) : (
+				<>
+					<div className={styles['header-bar']}>
+						<Header />
+					</div>
+					<div className={styles.container}>
+						<Outlet />
+						<Footer />
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
