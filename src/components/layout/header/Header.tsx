455e666a29d5af4451f4ca2a4fae5be6ebcddb18
Weiss Bai <whitesgr03@gmail.com>
2026-01-05 13:51:03 +0800

    feat!: update logo text

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
 	return (
 		<header className={styles.header}>
 			<a className={styles.link} href={import.meta.env.VITE_HELOG_URL}>
-				<h1 className={styles.logo}>HeLog Account</h1>
+				<h1 className={styles.logo}>HeLog</h1>
 			</a>
 		</header>
 	);
