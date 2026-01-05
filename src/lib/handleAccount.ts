d6377a0d890e62743ebf403cfbae4f89a6b33eff
Weiss Bai <whitesgr03@gmail.com>
2026-01-05 14:46:05 +0800

    feat: create the requestRegister method

--- src/lib/handleAccount.ts
+++ src/lib/handleAccount.ts
@@ -1,4 +1,5 @@
 import { handleFetch } from './handleFetch';
+import { type SignUpSchema } from '../components/pages/Account/SignUp';
 import { type SignInSchema } from '../components/pages/Account/SignIn';
 
 const URL = `${import.meta.env.VITE_RESOURCE_URL}/account`;
@@ -15,4 +16,20 @@ export const login = async (signal: AbortSignal, formFields: SignInSchema) => {
 	};
 
 	return await handleFetch(`${URL}/login`, options, [302, 401, 400]);
+};
+
+export const requestRegister = async (
+	signal: AbortSignal,
+	formFields: SignUpSchema,
+) => {
+	const options: RequestInit = {
+		method: 'POST',
+		signal,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(formFields),
+	};
+
+	return await handleFetch(`${URL}/requestRegister`, options, [400]);
 };
