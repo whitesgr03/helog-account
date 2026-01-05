083e84e2c5299d24179c1b05c7c8295fc88eb444
Weiss Bai <whitesgr03@gmail.com>
2026-01-05 13:32:37 +0800

    feat: federation component receive the props

--- src/components/pages/Account/Federation.tsx
+++ src/components/pages/Account/Federation.tsx
@@ -3,12 +3,13 @@ import styles from './Federation.module.css';
 import googleIcon from '../../../assets/google.png';
 import facebookIcon from '../../../assets/facebook.png';
 
-export const Federation = () => {
+export const Federation = ({ onLoading }: { onLoading: () => void }) => {
 	return (
 		<div className={styles.federation}>
 			<a
 				href={`${import.meta.env.VITE_RESOURCE_URL}/account/login/google`}
 				className={styles['federation-link']}
+				onClick={onLoading}
 			>
 				<div className={styles['federation-icon']}>
 					<img src={googleIcon} alt="Google icon" />
@@ -18,6 +19,7 @@ export const Federation = () => {
 			<a
 				href={`${import.meta.env.VITE_RESOURCE_URL}/account/login/facebook`}
 				className={styles['federation-link']}
+				onClick={onLoading}
 			>
 				<div className={styles['federation-icon']}>
 					<img src={facebookIcon} alt="Facebook icon" />
