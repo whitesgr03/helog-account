8a8ca2f449ab5ed22adb26afadeb70cca97bf7fe
Weiss Bai <whitesgr03@gmail.com>
2026-01-23 14:03:46 +0800

    test: create setup file

---
+++ src/setup.ts
@@ -0,0 +1,8 @@
+import { vi, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+
+afterEach(() => {
+	vi.resetAllMocks();
+	cleanup();
+});
