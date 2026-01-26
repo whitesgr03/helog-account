ecd5d110f54d5862062074fd66a0c8d05341f7cf
Weiss Bai <whitesgr03@gmail.com>
2026-01-26 22:14:56 +0800

    chore: add headers property

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,18 @@ export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 8001,
+		headers: {
+			'X-Content-Type-Options': ' nosniff',
+			'Cross-Origin-Embedder-Policy': 'credentialless',
+			'X-Frame-Options': 'DENY',
+			'Cross-Origin-Resource-Policy': 'same-origin',
+			'Referrer-Policy': 'no-referrer',
+			'X-XSS-Protection': '0',
+			'X-DNS-Prefetch-Control': 'off',
+			'Strict-Transport-Security':
+				'max-age=63072000; includeSubDomains preload',
+			'Content-Security-Policy-Report-Only': `default-src 'none'; connect-src 'self' localhost:4000; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com/css2; font-src https:; img-src 'self' https: data:; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests`,
+		},
 	},
 	test: {
 		globals: true,
